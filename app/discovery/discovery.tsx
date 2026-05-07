31302fc15b8c4703a3149a98257f9318d6c10ada
Lakmi <lakmi94@gmail.com>
2026-05-07 14:02:29 +0200

    add discovery page

---
+++ app/discovery/discovery.tsx
@@ -0,0 +1,10 @@
+export default function Discovery() {
+    return (
+        <div className="flex flex-col flex-1 items-center w-full bg-zinc-50 font-sans text-gray-800">
+            <h1 className="text-3xl font-bold mt-8">Archive</h1>
+            <p className="mt-4 text-lg text-gray-600">
+              Discovery: Hyper-search & Academic Gallery
+            </p>
+        </div>
+    );
+}
